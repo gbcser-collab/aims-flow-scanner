2b7dd19f54604d350f49d55bb1d471b21a98ea08
gbcser-collab <gbcser@gmail.com>
2026-09-15 19:37:03 +0200

    Scroll to lazy history section in widget smoke test

--- test/app_smoke_test.dart
+++ test/app_smoke_test.dart
@@ -1,5 +1,6 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:aims_flow_scanner/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('AIMS Flow Smart Scanner v0.7 home renders', (tester) async {
@@ -10,6 +11,12 @@ void main() {
     expect(find.text('AIMS FLOW • SMART • v0.7'), findsOneWidget);
     expect(find.text('CMR Scanner'), findsOneWidget);
     expect(find.text('Smart Scan indítása'), findsOneWidget);
+
+    await tester.scrollUntilVisible(
+      find.text('Legutóbbi mentések'),
+      500,
+      scrollable: find.byType(Scrollable).first,
+    );
     expect(find.text('Legutóbbi mentések'), findsOneWidget);
   });
 }
