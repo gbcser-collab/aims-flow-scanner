f0519f819dada9e8331e9962df065a4e4ec91d5b
gbcser-collab <gbcser@gmail.com>
2026-09-15 19:16:59 +0200

    NailFit v0.7: update smoke test for premium home

--- test/app_smoke_test.dart
+++ test/app_smoke_test.dart
@@ -3,14 +3,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:nailfit/main.dart';
 
 void main() {
-  testWidgets('NAILFIT v0.6 home screen renders', (tester) async {
+  testWidgets('NAILFIT v0.7 premium home renders', (tester) async {
     await tester.pumpWidget(const NailFitApp());
     await tester.pumpAndSettle();
 
     expect(find.byType(NailFitHome), findsOneWidget);
     expect(find.text('NAIL'), findsOneWidget);
     expect(find.text('FIT'), findsOneWidget);
-    expect(find.text('Találd meg.\nPróbáld fel. Szeresd.'), findsOneWidget);
-    expect(find.byType(CustomScrollView), findsOneWidget);
+    expect(find.text('a hozzád illő\nkörmöket.'), findsOneWidget);
+    expect(find.text('Készíts fotót'), findsOneWidget);
+    expect(find.byType(CustomScrollView), findsWidgets);
   });
 }
