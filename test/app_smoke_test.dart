5f01c1d58debd3062481eb5d2af6366762a33cb8
gbcser-collab <gbcser@gmail.com>
2026-09-15 18:08:37 +0200

    Update Smart Scanner widget smoke test

--- test/app_smoke_test.dart
+++ test/app_smoke_test.dart
@@ -2,12 +2,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:aims_flow_scanner/main.dart';
 
 void main() {
-  testWidgets('AIMS Flow home screen renders', (tester) async {
+  testWidgets('AIMS Flow Smart Scanner home renders', (tester) async {
     await tester.pumpWidget(const AimsFlowApp());
     await tester.pumpAndSettle();
 
-    expect(find.text('AIMS Flow Scanner'), findsOneWidget);
+    expect(find.text('AIMS Flow Smart Scanner'), findsOneWidget);
     expect(find.text('CMR Scanner'), findsOneWidget);
-    expect(find.text('Scanner megnyitása'), findsOneWidget);
+    expect(find.text('Smart Scan indítása'), findsOneWidget);
   });
 }
