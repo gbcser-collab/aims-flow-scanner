5f7156268b704e92a7b8d6653932c2e19a253359
gbcser-collab <gbcser@gmail.com>
2026-09-15 19:23:29 +0200

    NailFit v0.7.1: switch to robust premium screen engine

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'nailfit_app.dart';
-export 'nailfit_app.dart';
+import 'nailfit_app_v2.dart';
+export 'nailfit_app_v2.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
