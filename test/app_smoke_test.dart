72cedbddd9bc50a7ac5040000af8560e3f05d9ba
gbcser-collab <gbcser@gmail.com>
2026-09-15 19:32:27 +0200

    Update home smoke test for v0.7

--- test/app_smoke_test.dart
+++ test/app_smoke_test.dart
@@ -2,12 +2,14 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:aims_flow_scanner/main.dart';
 
 void main() {
-  testWidgets('AIMS Flow Smart Scanner home renders', (tester) async {
+  testWidgets('AIMS Flow Smart Scanner v0.7 home renders', (tester) async {
     await tester.pumpWidget(const AimsFlowApp());
     await tester.pumpAndSettle();
 
     expect(find.text('AIMS Flow Smart Scanner'), findsOneWidget);
+    expect(find.text('AIMS FLOW • SMART • v0.7'), findsOneWidget);
     expect(find.text('CMR Scanner'), findsOneWidget);
     expect(find.text('Smart Scan indítása'), findsOneWidget);
+    expect(find.text('Legutóbbi mentések'), findsOneWidget);
   });
 }
