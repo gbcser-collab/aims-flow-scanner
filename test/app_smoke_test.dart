ab72a795869d2a4fb0537a4ab6df2f055f8fda65
gbcser-collab <gbcser@gmail.com>
2026-09-15 18:47:03 +0200

    NailFit v0.6: make viewport smoke test lazy-list safe

--- test/app_smoke_test.dart
+++ test/app_smoke_test.dart
@@ -10,8 +10,6 @@ void main() {
     expect(find.text('NAIL'), findsOneWidget);
     expect(find.text('FIT'), findsOneWidget);
     expect(find.text('Találd meg.\nPróbáld fel. Szeresd.'), findsOneWidget);
-    expect(find.text('Neked ajánljuk'), findsOneWidget);
-    expect(find.text('Kedvencekhez'), findsOneWidget);
-    expect(find.text('Look mentése'), findsOneWidget);
+    expect(find.byType(CustomScrollView), findsOneWidget);
   });
 }
