b580b380ac1bf3b3952431ead5abcfb864d52ca4
gbcser-collab <gbcser@gmail.com>
2026-09-18 22:19:16 +0200

    Use FragmentActivity for system biometric unlock

--- native/android/MainActivity.kt
+++ native/android/MainActivity.kt
@@ -4,11 +4,11 @@ import android.app.role.RoleManager
 import android.content.Intent
 import android.os.Build
 import android.provider.Settings
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
     private val channelName = "hu.logisticaims.aims_flow/hands_free"
     private var handsFreeChannel: MethodChannel? = null
 
